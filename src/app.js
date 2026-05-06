d77b24b9984a2b488961d43316e283dbec332ea8
ENZO332 <e.gustavojuarez@gmail.com>
2026-05-06 14:23:38 -0300

    fix: enable CORS

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const eventRoutes = require("./routes/eventRoutes");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
